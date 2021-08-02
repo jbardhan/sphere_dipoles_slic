a5d5cc38adba53fea68bc2023107ef76049e00b8
Jay Bardhan <j.bardhan@neu.edu>
2021-08-01 20:47:33 -0400

    Initial data files

---
+++ salt_bridges/sweep_rotations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os, sys, subprocess, argparse, re, logging, errno
+import numpy as np
+import mymm
+
+parser = argparse.ArgumentParser(description="This program generates a set of rotated and possibly translated versions of the input PQR.", prog = sys.argv[0])
+
+parser.add_argument('--pqr', metavar = 'molecule.pqr')
+parser.add_argument('--iscentered', metavar = 'True if molecule is already centered at origin', default = True)
+parser.add_argument('--numrotations', metavar = 'Number of rotations (2 means 0 and 180)', type = int, default = 2)
+parser.add_argument('--translate', metavar = 'How far to translate along +X', type = float, default = 0.0)
+
+args = parser.parse_args(sys.argv[1:])
+
+samplegroup = mymm.Molecule()
+samplegroup.read_pqr(args.pqr)
+
+numbers = np.linspace(0.0, 180.0, args.numrotations)
+#print(numbers)
+
+for i in numbers:
+    print("Current rotation is " + str(i) + ".\n")
+
+    rotatedgroup = mymm.Molecule(samplegroup.atoms)
+    rotatedgroup.rotateZ(i)
+    rotatedgroup.write_apbs_pqr("foo_" + str(i) + ".pqr")
+    del rotatedgroup
+
+    
