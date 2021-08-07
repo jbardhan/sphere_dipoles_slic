030c9306f162ee8d140e76f44600962952c67936
Jaydeep Bardhan <jaydeep.bardhan@pnnl.gov>
2021-08-07 15:19:31 -0400

    added ability to translate PQR, and changed arg parser options to --inputpqr and --output

--- salt_bridges/sweep_rotations.py
+++ salt_bridges/sweep_rotations.py
@@ -6,7 +6,8 @@ import mymm
 
 parser = argparse.ArgumentParser(description="This program generates a set of rotated and possibly translated versions of the input PQR.", prog = sys.argv[0])
 
-parser.add_argument('--pqr', metavar = 'molecule.pqr')
+parser.add_argument('--inputpqr', metavar = 'input_molecule.pqr')
+parser.add_argument('--output', metavar = 'output_molecule_base_for_filenames')
 parser.add_argument('--iscentered', metavar = 'True if molecule is already centered at origin', default = True)
 parser.add_argument('--numrotations', metavar = 'Number of rotations (2 means 0 and 180)', type = int, default = 2)
 parser.add_argument('--translate', metavar = 'How far to translate along +X', type = float, default = 0.0)
@@ -14,7 +15,7 @@ parser.add_argument('--translate', metavar = 'How far to translate along +X', ty
 args = parser.parse_args(sys.argv[1:])
 
 samplegroup = mymm.Molecule()
-samplegroup.read_pqr(args.pqr)
+samplegroup.read_pqr(args.inputpqr)
 
 numbers = np.linspace(0.0, 180.0, args.numrotations)
 #print(numbers)
@@ -24,7 +25,8 @@ for i in numbers:
 
     rotatedgroup = mymm.Molecule(samplegroup.atoms)
     rotatedgroup.rotateZ(i)
-    rotatedgroup.write_apbs_pqr("foo_" + str(i) + ".pqr")
+    rotatedgroup.translate(x=args.translate,y=0.0,z=0.0)
+    rotatedgroup.write_apbs_pqr(args.output + str(i) + ".pqr")
     del rotatedgroup
 
     
